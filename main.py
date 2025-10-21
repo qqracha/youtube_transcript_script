b94b2f10921078711a5cf2852b8894bb69d9d40b
qqracha <evaldyukhov@gmail.com>
2025-10-21 22:46:30 +0200

    add beautiful time format & add comments

--- main.py
+++ main.py
@@ -3,32 +3,27 @@ import yt_dlp
 
 video_id = "SkGW-Wcn9Vs" # use video id, not url
 video_url = f"https://www.youtube.com/watch?v={video_id}"
-
+ytt_api = YouTubeTranscriptApi()
 ydl_opts = {}
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:  # fetch video metadata by yt_dlp
     info = ydl.extract_info(video_url, download=False)
     title = info['title']
     author = info['uploader']
     duration_sec = info['duration']
 
-ytt_api = YouTubeTranscriptApi()
-transcription = ytt_api.fetch(video_id, languages=['en', 'ru']) # 
+transcription = ytt_api.fetch(video_id, languages=['en', 'ru']) # change to your languages
 with open('transcript.txt', "w", encoding="UTF-8") as f:
+    f.write(f'Video name: {title}\nAuthor: {author}\nDuration: {duration_sec}s\n')
+    f.write('------------------------------\n')
     for entry in transcription:
         start = entry.start
         end = entry.start + entry.duration
-        if start >= 60 and end >= 60:
-            m_start = int(start // 60)
-            s_start = int(start % 60)
-            m_end = int(end // 60)
-            s_end = int(end % 60)
-            time_str = f'{m_start}m{s_start}s - {m_end}m{s_end}s'
-        else:
-            time_str = f'{start:.1f}s - {end:.1f}s'
 
+        def fmt_time(t):
+            m, s = divmod(t, 60) # split total seconds t into minutes and seconds
+            return f"{int(m):02}:{s:04.1f}" # format like MM:SS.s with leading zeros
+
+        time_str = f"{fmt_time(start)} - {fmt_time(end)}"
         f.write(f'[{time_str}] | {entry.text}\n')
-    print(f'------------\ntext saved in transcript.txt!\nVideo name: {title}\nAuthor: {author}\nDuration: {duration_sec}s')
-# with open('transcript.txt', "w", encoding="UTF-8") as f:
-#     for entry in transcription:
-#         f.write(f'{entry.start} | {entry.text} | {entry.duration}\n')
-#     print('text saved in transcript.txt!')
+    print(f'------------\nTranscript saved, check transcript.txt!\nVideo name: {title}\nAuthor: {author}\nDuration: {duration_sec}s')
